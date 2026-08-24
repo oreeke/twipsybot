edab70fb02df5a11771074a6fd46949a37911c2b
OREEkE <oreeke@live.com>
2026-08-24 22:28:18 +0800

    fix: plugin loading path

--- twipsybot/plugin/manager.py
+++ twipsybot/plugin/manager.py
@@ -167,7 +167,7 @@ class PluginManager:
 
     @staticmethod
     def _load_plugin_module(plugin_dir: Path, plugin_file: Path):
-        project_root = str(plugin_dir.parent.resolve())
+        project_root = str(plugin_dir.parent.parent.resolve())
         if project_root not in sys.path:
             sys.path.insert(0, project_root)
         spec = importlib.util.spec_from_file_location(
